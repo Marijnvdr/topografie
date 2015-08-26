f3335683fe15e51f90e1c46e7b16ca2419ddd158
Marijnvdr <mvdruijt@gmail.com>
2015-08-26 11:19:32 +0200

    Using JSONAPI adapter and serializer

--- app/serializers/application.js
+++ app/serializers/application.js
@@ -1,12 +1,13 @@
 import DS from 'ember-data';
 
-export default DS.RESTSerializer.extend({
-	isNewSerializerAPI: true,
-  
+export default DS.JSONAPISerializer.extend({
   normalizeResponse: function(store, primaryModelClass, payload, id, requestType) {
-  	payload.id = id;
-  	let newPayload = {};
-  	newPayload[primaryModelClass.modelName] = payload;
+    let data = {};
+    data.id = id;
+    data.type = primaryModelClass.modelName;
+    data.attributes = payload; 
+    let newPayload = {};
+    newPayload.data = data;
   	return this._super(store, primaryModelClass, newPayload, id, requestType);
   },
 
