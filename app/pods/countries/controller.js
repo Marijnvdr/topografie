d6d72474dbca47741e13cb07674c92d3ac1bbed9
mvdr <m.ruijt@niposoftware.com>
2016-03-16 20:24:27 +0100

    not working yet

--- app/pods/countries/controller.js
+++ app/pods/countries/controller.js
@@ -1,4 +1,17 @@
 import Ember from 'ember';
+import pagedArray from 'ember-cli-pagination/computed/paged-array';
 
-export default Ember.Controller.extend({
+export default Ember.ArrayController.extend({
+  queryParams: ["page", "perPage"],
+
+  // binding the property on the paged array
+  // to the query params on the controller
+  pageBinding: "content.page",
+  perPageBinding: "content.perPage",
+  totalPagesBinding: "content.totalPages",
+
+  // set default values, can cause problems if left out
+  // if value matches default, it won't display in the URL
+  page: 1,
+  perPage: 10  
 });
--- app/pods/countries/route.js
+++ app/pods/countries/route.js
@@ -1,11 +1,20 @@
 import Ember from 'ember';
+import RouteMixin from 'ember-cli-pagination/remote/route-mixin';
 
-export default Ember.Route.extend({
-  model: function() {
+export default Ember.Route.extend(RouteMixin, {
+  model: function(params) {
+    params.searchTerm = '';
+    params.paramMapping = {
+      perPage: "limit",
+      page: "offset"
+    };
+    return this.findPaged('country', params);
+    /*
     return this.store.queryRecord('country', {
       searchTerm: '',
       offset: 67,
       limit: 10
-    });      
+    });
+    */      
   }
 });
